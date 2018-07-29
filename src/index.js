70e19276825e93d3d0bb5d63944da623aa656a45
Kukuruzko <dmitriy.okhrimovich@gmail.com>
2018-07-30 00:28:37 +0300

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import app from './reducers/app';
+import App from './components/app';
+
+ReactDOM.render(
+	<Provider store={createStore(app)}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
