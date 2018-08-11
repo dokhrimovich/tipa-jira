946c3058950c50605e08564ad5250d47d38462d2
Kukuruzko <dmitriy.okhrimovich@gmail.com>
2018-08-11 18:41:08 +0300

    add router to task-editor

--- src/js/components/grid/gridColumn.js
+++ src/js/components/grid/gridColumn.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import TaskItemContainer from '../../containers/taskItemContainer';
+import TaskItemContainer from 'js/containers/taskItemContainer';
 
 class GridColumn extends React.Component {
     render() {
         let { config, tasks } = this.props;
 
         return (
-            <div className="grid-column">
+            <div className="grid-column columns">
                 <div className="grid-column_header"><header>{config.title}</header></div>
-                <div className="grid-column_content">
+                <div className="grid-column_content columns">
                     {tasks.map((task) => {
                         return (<TaskItemContainer key={task.id} task={task}></TaskItemContainer>);
                     })}
